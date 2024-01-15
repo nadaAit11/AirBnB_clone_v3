85c01effa44718e140a316d2690969d9d909e80c
nada <nadaait171@gmail.com>
2024-01-15 21:49:20 +0000

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -15,6 +15,8 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
 # Import your specific views
 # from api.v1.views.view1 import *
 # from api.v1.views.view2 import *
