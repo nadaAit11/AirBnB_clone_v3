206743cf43ff665e57a8ac01fb8dc69abd29c858
nada <nadaait171@gmail.com>
2024-01-15 20:12:08 +0000

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
 # Import your specific views
 # from api.v1.views.view1 import *
 # from api.v1.views.view2 import *
