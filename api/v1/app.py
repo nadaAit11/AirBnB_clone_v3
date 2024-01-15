3ad86dc9425c435073ddd05dd56336b6ef842e3f
NEAZYIT <touihnazih28@gmail.com>
2024-01-15 06:48:41 -0800

    Add main application module for HBNB API with Flask, Blueprint, and error handling.

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Main application module for HBNB API
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -18,6 +18,15 @@ def teardown(exception):
     Method to handle teardown of the app
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handler for 404 errors that returns a
+    JSON-formatted 404 status code response.
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
