f4d4590d9b5894a022082fb9b54ea23bff77ea01
NEAZYIT <touihnazih28@gmail.com>
2024-01-15 06:59:10 -0800

    Implemented a new view for State objects in states.py, supporting default RESTful API actions. Utilized the to_dict() method for JSON conversion. Updated init.py to import the new file. Handled actions such as retrieving the list of all State objects, retrieving a specific State object, deleting a State object, creating a new State, and updating a State object. Validated input data, handled errors, and provided appropriate HTTP responses. Demonstrated functionality through cURL commands with successful results on the provided GitHub repository (AirBnB_clone_v3) in the specified files: api/v1/views/states.py and api/v1/views/init.py.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,6 +10,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import your specific views using the Blueprint instance
 from api.v1.views.index import *
+from api.v1.views.states import *
 
 # Import your specific views
 # from api.v1.views.view1 import *
