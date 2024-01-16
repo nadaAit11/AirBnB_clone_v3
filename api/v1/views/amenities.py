be7dab9ef207db35acbd2a4a07b8487f07cd0618
NEAZYIT <touihnazih28@gmail.com>
2024-01-15 16:51:15 -0800

    fix bugs

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,116 +1,76 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+Amenity view module
 """
+
+from flask import jsonify, request, abort
 from api.v1.views import app_views
-from flask import abort, jsonify, request
-from flasgger.utils import swag_from
-from models import storage, CNC
-from os import environ
-STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
+from models import storage
+from models.amenity import Amenity
 
 
-@swag_from('swagger_yaml/places_by_city.yml', methods=['GET', 'POST'])
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
-def places_per_city(city_id=None):
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
     """
-        places route to handle http method for requested places by city
+    Retrieves the list of all Amenity objects
     """
-    city_obj = storage.get('City', city_id)
-    if city_obj is None:
-        abort(404, 'Not found')
-
-    if request.method == 'GET':
-        all_places = storage.all('Place')
-        city_places = [obj.to_json() for obj in all_places.values()
-                       if obj.city_id == city_id]
-        return jsonify(city_places)
-
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        user_id = req_json.get("user_id")
-        if user_id is None:
-            abort(400, 'Missing user_id')
-        user_obj = storage.get('User', user_id)
-        if user_obj is None:
-            abort(404, 'Not found')
-        if req_json.get("name") is None:
-            abort(400, 'Missing name')
-        Place = CNC.get("Place")
-        req_json['city_id'] = city_id
-        new_object = Place(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
+    return jsonify([amenity.to_dict() for amenity in
+                    storage.all(Amenity).values()])
 
 
-@swag_from('swagger_yaml/places_id.yml', methods=['GET', 'DELETE', 'PUT'])
-@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'])
-def places_with_id(place_id=None):
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity(amenity_id):
     """
-        places route to handle http methods for given place
+    Retrieves a Amenity object
     """
-    place_obj = storage.get('Place', place_id)
-    if place_obj is None:
-        abort(404, 'Not found')
-
-    if request.method == 'GET':
-        return jsonify(place_obj.to_json())
-
-    if request.method == 'DELETE':
-        place_obj.delete()
-        del place_obj
-        return jsonify({}), 200
-
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        place_obj.bm_update(req_json)
-        return jsonify(place_obj.to_json()), 200
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
 
 
-@app_views.route('/places_search', methods=['POST'])
-def places_search():
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
     """
-        places route to handle http method for request to search places
+    Deletes a Amenity object
     """
-    all_places = [p for p in storage.all('Place').values()]
-    req_json = request.get_json()
-    if req_json is None:
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    """
+    Creates a Amenity
+    """
+    if not request.get_json():
         abort(400, 'Not a JSON')
-    states = req_json.get('states')
-    if states and len(states) > 0:
-        all_cities = storage.all('City')
-        state_cities = set([city.id for city in all_cities.values()
-                            if city.state_id in states])
-    else:
-        state_cities = set()
-    cities = req_json.get('cities')
-    if cities and len(cities) > 0:
-        cities = set([
-            c_id for c_id in cities if storage.get('City', c_id)])
-        state_cities = state_cities.union(cities)
-    amenities = req_json.get('amenities')
-    if len(state_cities) > 0:
-        all_places = [p for p in all_places if p.city_id in state_cities]
-    elif amenities is None or len(amenities) == 0:
-        result = [place.to_json() for place in all_places]
-        return jsonify(result)
-    places_amenities = []
-    if amenities and len(amenities) > 0:
-        amenities = set([
-            a_id for a_id in amenities if storage.get('Amenity', a_id)])
-        for p in all_places:
-            p_amenities = None
-            if STORAGE_TYPE == 'db' and p.amenities:
-                p_amenities = [a.id for a in p.amenities]
-            elif len(p.amenities) > 0:
-                p_amenities = p.amenities
-            if p_amenities and all([a in p_amenities for a in amenities]):
-                places_amenities.append(p)
-    else:
-        places_amenities = all_places
-    result = [place.to_json() for place in places_amenities]
-    return jsonify(result)
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    amenity = Amenity(**request.get_json())
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_amenity(amenity_id):
+    """
+    Updates a Amenity object
+    """
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,107 +1,78 @@
 #!/usr/bin/python3
-""" Module containing User View """
+"""
+User view module
+"""
+
+from flask import jsonify, request, abort
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
 from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
-    """ Retrieves the list of all User objects.
-
-        Returns:
-            List of dictionaries representing User objects in JSON format.
     """
-    users = [user.to_dict() for user in storage.all("User").values()]
-    return jsonify(users)
+    Retrieves the list of all User objects
+    """
+    return jsonify([user.to_dict() for user in
+                   storage.all(User).values()])
 
 
-@app_views.route('/users/<string:user_id>', methods=['GET'],
+@app_views.route('/users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def get_user(user_id):
-    """ Retrieves a User object based on `user_id`.
-
-    Args:
-        user_id (str): The UUID4 string representing a User object.
-
-    Returns:
-        Dictionary represention of a User object in JSON format.
-        404 error if `user_id` is not linked to any User object.
     """
-    user_obj = storage.get("User", user_id)
-    if user_obj is None:
+    Retrieves a User object
+    """
+    user = storage.get(User, user_id)
+    if user is None:
         abort(404)
-    return jsonify(user_obj.to_dict())
+    return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+@app_views.route('/users/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_user(user_id):
-    """ Deletes a User object based on `user_id`.
-
-    Args:
-        user_id (str): The UUID4 string representing a User object.
-
-    Returns:
-        Returns an empty dictionary with the status code 200.
-        404 error if `user_id` is not linked to any User object.
     """
-    user_obj = storage.get("User", user_id)
-    if user_obj is None:
+    Deletes a User object
+    """
+    user = storage.get(User, user_id)
+    if user is None:
         abort(404)
-    user_obj.delete()
+    storage.delete(user)
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-def add_user():
-    """ Creates a User object using HTTP body request fields.
-
-    Returns:
-        Returns the new User object as a  dictionary in JSON format
-        with the status code 201.
-        400 error if HTTP body request is not a valid JSON or if the dictionary
-        passed does not contain the key `email` and/or `password`.
+def create_user():
     """
-    if request.json is None:
-        return "Not a JSON", 400
-    fields = request.get_json()
-    if fields.get('email') is None:
-        return "Missing email", 400
-    if fields.get('password') is None:
-        return "Missing password", 400
-    new_user = User(**fields)
-    new_user.save()
-    """ May need to call `get` on new_user for all attributes to show """
-    return jsonify(new_user.to_dict()), 201
+    Creates a User
+    """
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'email' not in request.get_json():
+        abort(400, 'Missing email')
+    if 'password' not in request.get_json():
+        abort(400, 'Missing password')
+    user = User(**request.get_json())
+    user.save()
+    return jsonify(user.to_dict()), 201
 
 
-@app_views.route('/users/<string:user_id>', methods=['PUT'],
+@app_views.route('/users/<user_id>', methods=['PUT'],
                  strict_slashes=False)
-def edit_user(user_id):
-    """ Edit a User object using `user_id` and HTTP body request fields.
-
-    Args:
-        user_id (str): The UUID4 string representing a User object.
-
-    Returns:
-        Returns the User object as a dictionary in JSON format with the
-        status code 200.
-        400 error if the HTTP body request is not a valid JSON.
-        404 error if `user_id` is not linked to any User object.
+def update_user(user_id):
     """
-    user_obj = storage.get("User", user_id)
-    if user_obj is None:
+    Updates a User object
+    """
+    user = storage.get(User, user_id)
+    if user is None:
         abort(404)
-    if request.json is None:
-        return "Not a JSON", 400
-    fields = request.get_json()
-    for key in fields:
-        if key in ['id', 'email', 'created_at', 'update_at']:
-            continue
-        if hasattr(user_obj, key):
-            setattr(user_obj, key, fields[key])
-    user_obj.save()
-    return jsonify(user_obj.to_dict()), 200
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    for key, value in request.get_json().items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
