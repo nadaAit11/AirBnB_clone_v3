2f94e8d6e5a2e53751e79a75403a494ea38fbac0
NEAZYIT <touihnazih28@gmail.com>
2024-01-15 17:05:34 -0800

    fix bugs

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,17 @@ Main application module for HBNB API
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+# Create a CORS instance
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
